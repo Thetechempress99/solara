b30b448e3a1b992b1a61dce2caec334831bc1835
Thetechempress99 <blessing18020@gmail.com>
2026-06-19 13:32:26 +0100

    first commit of the top_up app

---
+++ gardol_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GardolAppConfig(AppConfig):
+    name = 'gardol_app'
---
+++ gardol_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+
+# Create your views here.
+
+def login(request):
+    return render(request, 'login.html')
+def dashboard(request):
+    return render(request, 'dashboard.html')
+def home(request):
+    return render(request, 'index.html')
